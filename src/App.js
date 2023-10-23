fd523e68912ccef1e25bac60c9bb9615963d3e38
pratikpatel30 <pratikpatel3060@gmai.com>
2023-10-23 09:42:08 +0530

    create login page

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
   return (
     <div className='App'>
       <Home />
-      {/* <Login />
-      <Register /> */}
+      <Login />
+      <Register />
     </div>
   );
 }
--- src/components/loginpage/Login.js
+++ src/components/loginpage/Login.js
@@ -3,8 +3,13 @@ import './Login.css'
 
 function Login() {
   return (
-    <div>
-        <h1>Login Page</h1>
+    <div className='login'>
+        <h1>Login</h1>
+        <input type="text" placeholder='Enter your Email'></input>
+        <input type="password" placeholder='Enter your Password'></input>
+        <div className="button">Login</div>
+        <div>or</div>
+        <div className="button">Register</div>
     </div>
   )
 }
