0f94c0ad5f9c867e4908d2897e53efa78d5173a7
pratikpatel30 <pratikpatel3060@gmai.com>
2023-10-23 09:47:56 +0530

    create register page

--- src/components/registerpage/Register.js
+++ src/components/registerpage/Register.js
@@ -3,8 +3,15 @@ import './Register.css'
 
 function Register() {
   return (
-    <div>
-        <h1>Register Page</h1>
+    <div className='register'>
+        <h1>Register</h1>
+        <input type="text" placeholder='Enter your Name'></input>
+        <input type="text" placeholder='Enter your Eamil'></input>
+        <input type="password" placeholder='Enter your Password'></input>
+        <input type="password" placeholder='Confirm your Password'></input>
+        <div className="button">Register</div>
+        <div>or</div>
+        <div className="button">Login</div>
     </div>
   )
 }
